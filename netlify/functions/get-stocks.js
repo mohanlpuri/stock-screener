996fd6a2e6fb3e6787289a1fbcfcb7ce14a88eea
Mohan Puri <mohanlpuri@yahoo.com>
2026-06-13 18:27:27 -0400

    Test hardcoded sheet ID

--- netlify/functions/get-stocks.js
+++ netlify/functions/get-stocks.js
@@ -40,7 +40,9 @@ exports.handler = async function(event) {
       return data.access_token 
     }
 
-    const SHEET_ID = process.env.GOOGLE_SHEET_ID_TICKERS
+    //const SHEET_ID = process.env.GOOGLE_SHEET_ID_TICKERS
+    const SHEET_ID = '1jEewUVyxHH-vA0qboYE7fGSn22UO7a5fMkwjZ7IXfOE'
+
 
     // ── Action: Save to Watchlist ────────────────────────────────────────────
     if (body.action === 'saveToWatchlist') {
